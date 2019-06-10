98fadc7c7d1efbf2cfff713afa14dbae418e4d15
satori06 <satori06@att.net>
2019-06-10 12:22:31 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,34 +2,34 @@ function makeGrid() {
    console.log("makeGrid is running!")
 
 // Select size input
+	
 var canvas, cell, gridHeight, gridWidth, rows;
+	
+canvas = $('#pixel_canvas');
+gridHeight = $('#input_height').val();
+gridWidth = $('#input_width').val();
   
-   canvas = $('#pixel_canvas');
-   gridHeight = $('#input_height').val();
-   gridWidth = $('#input_width').val();
+canvas.children().remove()
   
-   canvas.children().remove()
+for (x = 0; x < gridHeight; x++) {
+canvas.append('<tr></tr>');
+}  
+
+rows = $('tr');
   
-   for (x = 0; x < gridHeight; x++) {
-   canvas.append('<tr></tr>');
-   }
+for (y = 0; y < gridWidth; y++) {
+rows.append('<td></td>');
+}
   
-   rows = $('tr');
-  
-   for (y = 0; y < gridWidth; y++) {
-   rows.append('<td></td>');
-   }
-  
-   cell = canvas.find('td');
+cell = canvas.find('td');
 	
  // When td is clicked by the user, change color of td
-   cell.click(function() {
-       // Select color input
-       console.log("changeColor is running!");
-       var color;
-       color = $("#colorPicker").val();
-       $(this).attr('bgcolor', color);
-	   
+cell.click(function() {
+    // Select color input
+    console.log("changeColor is running!");
+    var color;
+    color = $("#colorPicker").val();
+    $(this).attr('bgcolor', color);	   
  });
     
 }
